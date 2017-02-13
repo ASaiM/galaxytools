a90a7720c86e0fc6ab05e71d2eaeb1eaf31d8454
Bérénice Batut <bebatut@gmail.com>
2017-02-13 14:59:52 +0100

    Fix flake8 for tools/humann2/transform_json_to_pkl.py

--- tools/humann2/transform_json_to_pkl.py
+++ tools/humann2/transform_json_to_pkl.py
@@ -6,13 +6,15 @@ import bz2
 import json
 import argparse
 
+
 def transform_json_to_pkl(args):
     with open(args.json_input, 'r') as json_file:
         json_str = json_file.read()
         metadata = json.loads(json_str)
 
         for marker in metadata["markers"]:
-            metadata["markers"][marker]["ext"] = set(metadata["markers"][marker]["ext"])
+            a_set = set(metadata["markers"][marker]["ext"])
+            metadata["markers"][marker]["ext"] = a_set
 
     pkl_output = bz2.BZ2File(args.pkl_output, 'w')
     pickle.dump(metadata, pkl_output, pickle.HIGHEST_PROTOCOL)
@@ -22,7 +24,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--json_input', required=True)
     parser.add_argument('--pkl_output', required=True)
-    
     args = parser.parse_args()
 
     transform_json_to_pkl(args)
