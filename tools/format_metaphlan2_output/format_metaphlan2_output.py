2cc71b230101205641d7fafa822d4ab3d398066a
Bérénice Batut <bebatut@gmail.com>
2020-09-14 11:43:04 +0200

    Optimization of python script

--- tools/format_metaphlan2_output/format_metaphlan2_output.py
+++ tools/format_metaphlan2_output/format_metaphlan2_output.py
@@ -4,54 +4,49 @@
 import argparse
 
 
-taxo_level_correspondance = {}
-taxo_level_correspondance['k'] = 'kingdom'
-taxo_level_correspondance['p'] = 'phylum'
-taxo_level_correspondance['c'] = 'class'
-taxo_level_correspondance['o'] = 'order'
-taxo_level_correspondance['f'] = 'family'
-taxo_level_correspondance['g'] = 'genus'
-taxo_level_correspondance['s'] = 'species'
-taxo_level_correspondance['t'] = 'strains'
-
+taxo_level_corresp = {
+    'k': 'kingdom',
+    'p': 'phylum',
+    'c': 'class',
+    'o': 'order',
+    'f': 'family',
+    'g': 'genus',
+    's': 'species',
+    't': 'strains'}
+    
 
 def write_taxo_abundance(output_files, level, taxo, abundance):
-    if level not in taxo_level_correspondance:
+    if level not in taxo_level_corresp:
         raise ValueError(level + ' is not a know taxonomic level')
-    output_files[taxo_level_correspondance[level]].write(taxo + '\t')
-    output_files[taxo_level_correspondance[level]].write(abundance + '\n')
+    f_n = taxo_level_corresp[level]
+    output_files[f_n].write(taxo + '\t')
+    output_files[f_n].write(abundance + '\n')
 
 
 def format_metaphlan2_output(args):
-    taxo_levels_abund_files = {}
-    taxo_levels_abund_files['kingdom'] = open(args.kingdom_abundance_file, 'w')
-    taxo_levels_abund_files['phylum'] = open(args.phylum_abundance_file, 'w')
-    taxo_levels_abund_files['class'] = open(args.class_abundance_file, 'w')
-    taxo_levels_abund_files['order'] = open(args.order_abundance_file, 'w')
-    taxo_levels_abund_files['family'] = open(args.family_abundance_file, 'w')
-    taxo_levels_abund_files['genus'] = open(args.genus_abundance_file, 'w')
-    taxo_levels_abund_files['species'] = open(args.species_abundance_file, 'w')
-    taxo_levels_abund_files['strains'] = open(args.strains_abundance_file, 'w')
+    taxo_levels_abund_f = {
+        'kingdom': open(args.kingdom_abundance_file, 'w'),
+        'phylum': open(args.phylum_abundance_file, 'w'),
+        'class': open(args.class_abundance_file, 'w'),
+        'order': open(args.order_abundance_file, 'w'),
+        'family': open(args.family_abundance_file, 'w'),
+        'genus': open(args.genus_abundance_file, 'w'),
+        'species': open(args.species_abundance_file, 'w'),
+        'strains': open(args.strains_abundance_file, 'w')
+    }
 
-    for taxo_level_file in taxo_levels_abund_files:
-        string = taxo_level_file + '\t' + 'abundance\n'
-        taxo_levels_abund_files[taxo_level_file].write(string)
+    for taxo_level_f in taxo_levels_abund_f:
+        s = taxo_level_f + '\t' + 'abundance\n'
+        taxo_levels_abund_f[taxo_level_f].write(s)
 
-    with open(args.metaphlan2_output, 'r') as input_file:
-        with open(args.all_taxo_level_abundance_file, 'w') as output_file:
-            string = "kingdom\t"
-            string += "phylum\t"
-            string += "class\t"
-            string += "order\t"
-            string += "family\t"
-            string += "genus\t"
-            string += "species\t"
-            string += "strains\t"
-            string += "abundance\n"
-            output_file.write(string)
+    with open(args.metaphlan2_output, 'r') as input_f:
+        with open(args.all_taxo_level_abundance_file, 'w') as output_f:
+            s = "kingdom\tphylum\tclass\torder\tfamily\t"
+            s += "genus\tspecies\tstrains\tabundance\n"
+            output_f.write(s)
 
             levels_number = 8
-            for line in input_file.readlines():
+            for line in input_f.readlines():
                 if line.startswith("#"):
                     continue
 
@@ -63,24 +58,24 @@ def format_metaphlan2_output(args):
                 for level in split_taxo:
                     taxo = level.split('__')[1]
                     taxo = taxo.replace("_", " ")
-                    output_file.write(taxo + '\t')
+                    output_f.write(taxo + '\t')
 
                 for i in range(len(split_taxo), levels_number):
-                    output_file.write('\t')
+                    output_f.write('\t')
 
-                output_file.write(abundance + "\n")
+                output_f.write(abundance + "\n")
 
                 last_taxo_level = split_taxo[-1].split('__')
                 taxo = last_taxo_level[1].replace("_", " ")
                 level = last_taxo_level[0]
                 write_taxo_abundance(
-                    taxo_levels_abund_files,
+                    taxo_levels_abund_f,
                     level,
                     taxo,
                     abundance)
 
-    for taxo_level_file in taxo_levels_abund_files:
-        taxo_levels_abund_files[taxo_level_file].close()
+    for taxo_level_f in taxo_levels_abund_f:
+        taxo_levels_abund_f[taxo_level_f].close()
 
 
 if __name__ == '__main__':
