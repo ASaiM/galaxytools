dc55dc3b5275d1d6aac390698c0c6e0ab8fbf2f7
Bérénice Batut <bebatut@gmail.com>
2020-09-14 15:49:44 +0200

    Port to python 3 and improve wrapper

--- tools/compare_humann2_output/compare_humann2_output.py
+++ tools/compare_humann2_output/compare_humann2_output.py
@@ -4,12 +4,12 @@
 import argparse
 
 
-def extract_abundances(filepath, nb_charact_to_extract):
+def extract_abundances(fp, nb_charact_to_extract):
     abundances = {}
     more_abund_charact = []
     abund_sum = 0
-    with open(filepath, 'r') as abundance_file:
-        for line in abundance_file.readlines()[1:]:
+    with open(fp, 'r') as abundance_f:
+        for line in abundance_f.readlines()[1:]:
             split_line = line[:-1].split('\t')
             charact_id = split_line[0]
             abund = float(split_line[1])
@@ -51,111 +51,103 @@ def format_characteristic_name(all_name):
     return charact_id, char_name
 
 
-def write_more_abundant_charat(abundances, more_abund_charact,
-                               output_filepath):
-    with open(output_filepath, 'w') as output_file:
-        output_file.write('id\tname\t')
-        output_file.write('\t'.join(abundances.keys()) + '\n')
+def write_more_abundant_charat(abundances, more_abund_charact, output_fp):
+    with open(output_fp, 'w') as output_f:
+        output_f.write('id\tname\t%s\n' % '\t'.join(abundances.keys()))
 
         for mac in more_abund_charact:
             charact_id, charact_name = format_characteristic_name(mac)
-            output_file.write(charact_id + '\t' + charact_name)
+            output_f.write('%s\t%s' % (charact_id, charact_name))
             for sample in abundances:
                 abund = abundances[sample].get(mac, 0)
-                output_file.write('\t' + str(abund))
-            output_file.write('\n')
+                output_f.write('\t%s' % (abund))
+            output_f.write('\n')
 
 
-def extract_similar_characteristics(abund, sim_output_filepath,
-                                    specific_output_files):
-    sim_characteristics = set(abund[abund.keys()[0]].keys())
-    for sample in abund.keys()[1:]:
+def extract_similar_characteristics(abund, sim_output_fp, output_files):
+    abund_keys = list(abund)
+    sim_characteristics = set(abund[abund_keys[0]].keys())
+    for sample in abund_keys[1:]:
         sim_characteristics.intersection_update(abund[sample].keys())
-    print('Similar between all samples:' + str(len(sim_characteristics)))
+    print('Similar between all samples: %s' % len(sim_characteristics))
 
-    with open(sim_output_filepath, 'w') as sim_output_file:
-        sim_output_file.write('id\tname\t' + '\t'.join(abund.keys()) + '\n')
+    with open(sim_output_fp, 'w') as sim_output_f:
+        sim_output_f.write('id\tname\t%s\n' % '\t'.join(abund_keys))
         for charact in list(sim_characteristics):
             charact_id, charact_name = format_characteristic_name(charact)
-            sim_output_file.write(charact_id + '\t' + charact_name)
-            for sample in abund.keys():
-                sim_output_file.write('\t' + str(abund[sample][charact]))
-            sim_output_file.write('\n')
+            sim_output_f.write('%s\t%s' % (charact_id, charact_name))
+            for sample in abund_keys:
+                sim_output_f.write('\t%s' % abund[sample][charact])
+            sim_output_f.write('\n')
 
     print('Specific to samples:')
     diff_char = {}
-    for i in range(len(abund.keys())):
-        sample = abund.keys()[i]
-        string = ' ' + sample + ""
-        string += '    All:' + len(abund[sample].keys())
+    for i in range(len(abund_keys)):
+        sample = abund_keys[i]
+        print(' %s' % sample )
+        print('    All: %s' % len(abund[sample].keys()))
         diff_char[sample] = set(abund[sample].keys())
         diff_char[sample].difference_update(sim_characteristics)
-        string += '    Number of specific characteristics:'
-        string += str(len(diff_char[sample]))
-        string += '    Percentage of specific characteristics:',
         perc = 100*len(diff_char[sample])/(1.*len(abund[sample].keys()))
-        string += str(perc)
-        print(string)
+        print('    Number of specific characteristics: %s' % len(diff_char[sample]))
+        print('    Percentage of specific characteristics: %s' % perc)
 
         relative_abundance = 0
-        with open(specific_output_files[i], 'w') as output_file:
-            output_file.write('id\tname\tabundances\n')
+        with open(output_files[i], 'w') as output_f:
+            output_f.write('id\tname\tabundances\n')
             for charact in list(diff_char[sample]):
                 charact_id, charact_name = format_characteristic_name(charact)
-                output_file.write(charact_id + '\t' + charact_name + '\t')
-                output_file.write(str(abund[sample][charact]) + '\n')
+                output_f.write('%s\t%s' % (charact_id, charact_name))
+                output_f.write('%s\n' % abund[sample][charact])
                 relative_abundance += abund[sample][charact]
-        string = '    Relative abundance of specific characteristics('
-        string += relative_abundance
-        string += '):'
-        print(string)
+        print('    Relative abundance of specific characteristics: %s' % relative_abundance)
 
     return sim_characteristics
 
 
 def compare_humann2_output(args):
-    abundances = {}
+    abund = {}
     more_abund_charact = []
 
     for i in range(len(args.sample_name)):
-        abundances[args.sample_name[i]], mac = extract_abundances(
-            args.charact_input_file[i],
+        abund[args.sample_name[i]], mac = extract_abundances(
+            args.charact_input_fp[i],
             args.most_abundant_characteristics_to_extract)
         more_abund_charact += mac
 
     write_more_abundant_charat(
-        abundances,
+        abund,
         list(set(more_abund_charact)),
-        args.more_abundant_output_file)
+        args.more_abundant_output_fp)
     extract_similar_characteristics(
-        abundances,
-        args.similar_output_file,
-        args.specific_output_file)
+        abund,
+        args.similar_output_fp,
+        args.specific_output_fp)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--sample_name', required=True, action='append')
-    parser.add_argument('--charact_input_file', required=True, action='append')
+    parser.add_argument('--charact_input_fp', required=True, action='append')
     parser.add_argument(
         '--most_abundant_characteristics_to_extract',
         required=True,
         type=int)
-    parser.add_argument('--more_abundant_output_file', required=True)
-    parser.add_argument('--similar_output_file', required=True)
+    parser.add_argument('--more_abundant_output_fp', required=True)
+    parser.add_argument('--similar_output_fp', required=True)
     parser.add_argument(
-        '--specific_output_file',
+        '--specific_output_fp',
         required=True,
         action='append')
     args = parser.parse_args()
 
-    if len(args.sample_name) != len(args.charact_input_file):
+    if len(args.sample_name) != len(args.charact_input_fp):
         string = "Same number of values (in same order) are expected for "
-        string += "--sample_name and --charact_input_file"
+        string += "--sample_name and --charact_input_fp"
         raise ValueError(string)
-    if len(args.sample_name) != len(args.specific_output_file):
+    if len(args.sample_name) != len(args.specific_output_fp):
         string = "Same number of values (in same order) are expected for "
-        string += "--sample_name and --specific_output_file"
+        string += "--sample_name and --specific_output_fp"
         raise ValueError(string)
 
     compare_humann2_output(args)
